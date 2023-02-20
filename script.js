0afe88dd22eb728b3f3fa474e054af7c84564792
sebbex1337 <sefort@hotmail.dk>
2023-02-20 13:02:16 +0100

    Added js file and src in html

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Get all containers */
+const grumpy = document.querySelector("#grumpy_container");
+const fryingpan = document.querySelector("#fryingpanbird_container");
+const pinkbird = document.querySelector("#pinkbird_container");
+const punkbird = document.querySelector("#punkbird_container");
+const airplaneblue = document.querySelector("#airplaneblue_container");
+const airplanedarkblue = document.querySelector("#airplanedarkblue_container");
+
+/* Event listeners */
